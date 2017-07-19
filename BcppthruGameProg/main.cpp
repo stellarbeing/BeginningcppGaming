674a927fdefdaf3d3fa22b1499c5314e5d4b2c35
Rick Mynatt <asuperlongemailaddress@gmail.com>
2017-07-19 18:14:16 -0500

    git@github.com:stellarbeing/BeginningcppGaming.git

---
+++ BcppthruGameProg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BcppthruGameProg
+//
+//  Created by Rick on 7/19/17.
+//  Copyright © 2017 Rick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
