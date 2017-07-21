3a356a904e79189f08c383c7d3cec08bc13a3a16
Rick Mynatt <asuperlongemailaddress@gmail.com>
2017-07-21 13:26:35 -0500

    Test commit

--- BcppthruGameProg/main.cpp
+++ BcppthruGameProg/main.cpp
@@ -6,10 +6,27 @@
 //  Copyright © 2017 Rick. All rights reserved.
 //
 
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+
+{
+    const int ALIEN_POINTS = 150;
+    int aliensKilled = 10;
+    int score = aliensKilled * ALIEN_POINTS;
+    cout << "score: " << score << endl;
+    
+    enum difficulty {NOVICE, EASY, NORMAL, HARD, UNBEATABLE};
+    
+    difficulty myDifficulty = EASY;
+    
+    enum shipCost {FIGER_COST = 25, BOMBER_COST, CRUISER_COST = 50};
+    shipCost myShipCost = BOMBER_COST;
+    cout << "\nTo upgrade my ship to a Cruiser will cost "
+         << (CRUISER_COST - myShipCost) << " Resource Points. \n";
+    
     return 0;
 }
